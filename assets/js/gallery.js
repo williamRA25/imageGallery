4feeaf7c419b364d48f7298ed125e7b1eaf7266a
williamRA25 <williamrodriguezavila43@gmail.com>
2025-07-20 12:37:56 -0500

    add feature to render all Images when category param is not set

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,9 +1,3 @@
-const queryString = window.location.search;
-console.log(queryString);
-// ?category=forest
-const urlParams = new URLSearchParams(queryString);
-const category = urlParams.get("category");
-
 function findCategory(imageData, slug) {
     let foundCategory = null
   for (let index = 0; index < imageData.length; index++) {
@@ -15,13 +9,43 @@ function findCategory(imageData, slug) {
   }
   return foundCategory;
 }
-const imageCategory = findCategory(imagesData, category)
 
-// asignado valor que devuelve la funcion a la constante 
-const cardImagesHtml = renderCardImages(imageCategory.images)
+function getAllImages(imagesData) {
+  const allImages = [];
+  for (let index = 0; index < imagesData.length; index++) {
+    const imagesCategory = imagesData[index];
+
+    // en este punto estoy empujando todas las imagenes del objeto imagesCategory, la llave se llama images
+    // para acceder al valor de una llave se hace con el nombre del objeto seguido de un punto y el numbre de la llave asi
+    // por ejemplo si el objeto se llama miObjeto y ese objeto tiene una llave que se llame llave1 que tiene como valr un numero 1 
+    // para acceder solo hago miObjeto.llave1 lo cual me devolvera 1.      
+    allImages.push(...imagesCategory.images)
+  }
+  return allImages;
+}
+
+const queryString = window.location.search;
+// ?category=forest
+const urlParams = new URLSearchParams(queryString);
+const category = urlParams.get("category");
 
 // obtener el elemento html para manipularlo atravez del DOM
-const listImgContainer = document.getElementById("list-images")
+  const listImgContainer = document.getElementById("list-images")
 
-// asignar el html al elemento obtenido con la funcion getElementById
-listImgContainer.innerHTML = cardImagesHtml 
+// se valida que el parametro category que se recive a traves de la url tenga un valor 
+if (category !== null) {
+  const imageCategory = findCategory(imagesData, category)
+  
+  // asignado valor que devuelve la funcion a la constante 
+  const cardImagesHtml = renderCardImages(imageCategory.images)
+  
+  // asignar el html al elemento obtenido con la funcion getElementById
+  listImgContainer.innerHTML = cardImagesHtml 
+ 
+} else {
+  // else seginifica caso contrario es decir si la condision del if no se cumple se usa el codigo de dentro de este bloque 
+    const allImages = getAllImages(imagesData)
+    const cardImagesHtml = renderCardImages(allImages)
+    listImgContainer.innerHTML = cardImagesHtml 
+}   
+
