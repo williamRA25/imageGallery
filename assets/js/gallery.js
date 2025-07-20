d36f5a7c405366bed6cbcde924c2b25dd851e57e
williamRA25 <williamrodriguezavila43@gmail.com>
2025-07-20 15:54:42 -0500

    add feature to filter images by category and author

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,10 +1,10 @@
 function findCategory(imageData, slug) {
-    let foundCategory = null
+  let foundCategory = null;
   for (let index = 0; index < imageData.length; index++) {
     const imageCategory = imageData[index];
     if (slug === imageCategory.slug) {
-        foundCategory = imageCategory
-        break;  
+      foundCategory = imageCategory;
+      break;
     }
   }
   return foundCategory;
@@ -17,35 +17,121 @@ function getAllImages(imagesData) {
 
     // en este punto estoy empujando todas las imagenes del objeto imagesCategory, la llave se llama images
     // para acceder al valor de una llave se hace con el nombre del objeto seguido de un punto y el numbre de la llave asi
-    // por ejemplo si el objeto se llama miObjeto y ese objeto tiene una llave que se llame llave1 que tiene como valr un numero 1 
-    // para acceder solo hago miObjeto.llave1 lo cual me devolvera 1.      
-    allImages.push(...imagesCategory.images)
+    // por ejemplo si el objeto se llama miObjeto y ese objeto tiene una llave que se llame llave1 que tiene como valr un numero 1
+    // para acceder solo hago miObjeto.llave1 lo cual me devolvera 1.
+    allImages.push(...imagesCategory.images);
   }
   return allImages;
+}
+
+function getCategories(imagesData) {
+  const categories = [];
+  for (let index = 0; index < imagesData.length; index++) {
+    const category = imagesData[index];
+    categories.push({
+      label: category.category,
+      value: category.slug,
+    });
+  }
+  return categories;
+}
+
+function getAuthors(imageData) {
+  const allImages = getAllImages(imageData);
+  const uniqueAuthors = new Set();
+  for (let index = 0; index < allImages.length; index++) {
+    const image = allImages[index];
+    uniqueAuthors.add(image.author);
+  }
+  const authorsArray = [...uniqueAuthors];
+  return authorsArray.map((author) => {
+    return {
+      label: author,
+      value: author,
+    };
+  });
+}
+
+function setFilterOption(listOption, select) {
+  listOption.forEach((option) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = option.value;
+    optionElement.textContent = option.label;
+    select.appendChild(optionElement);
+  });
+}
+
+function filterImages(activeFilters, imageData) {
+  const allImages = getAllImages(imageData)
+  const author = activeFilters.author;
+  const category = activeFilters.category;
+  const search = activeFilters.search;
+
+  let filtered = allImages;
+  if (author) {
+    filtered = allImages.filter((image) => {
+      return image.author === author;
+    });
+  }
+
+  if (category) {
+    const categoryData = findCategory(imageData, category)
+    // se valida que categorydata sea un valor truthy, es decir que tenga un valor asignado 
+    if (categoryData) {
+      filtered = categoryData.images
+    }
+  }
+  return filtered;
 }
 
 const queryString = window.location.search;
 // ?category=forest
 const urlParams = new URLSearchParams(queryString);
-const category = urlParams.get("category");
+let category = urlParams.get("category");
+
+const activeFilters = {
+  author: "",
+  category: category,
+  search: "",
+};
+
+const filtered = filterImages(activeFilters, imagesData);
 
 // obtener el elemento html para manipularlo atravez del DOM
-  const listImgContainer = document.getElementById("list-images")
+const listImgContainer = document.getElementById("list-images");
 
-// se valida que el parametro category que se recive a traves de la url tenga un valor 
-if (category !== null) {
-  const imageCategory = findCategory(imagesData, category)
-  
-  // asignado valor que devuelve la funcion a la constante 
-  const cardImagesHtml = renderCardImages(imageCategory.images)
-  
+// asignado valor que devuelve la funcion a la constante
+const cardImagesHtml = renderCardImages(filtered);
+
+// asignar el html al elemento obtenido con la funcion getElementById
+listImgContainer.innerHTML = cardImagesHtml;
+
+const authors = getAuthors(imagesData);
+const categories = getCategories(imagesData);
+
+const selectAuthor = document.getElementById("author-filter");
+const selectCategory = document.getElementById("category-filter");
+
+setFilterOption(authors, selectAuthor);
+setFilterOption(categories, selectCategory);
+
+selectAuthor.addEventListener("change", (event) => {
+  activeFilters.author = event.target.value;
+  const filtered = filterImages(activeFilters, imagesData);
+  // asignado valor que devuelve la funcion a la constante
+  const cardImagesHtml = renderCardImages(filtered);
+
   // asignar el html al elemento obtenido con la funcion getElementById
-  listImgContainer.innerHTML = cardImagesHtml 
- 
-} else {
-  // else seginifica caso contrario es decir si la condision del if no se cumple se usa el codigo de dentro de este bloque 
-    const allImages = getAllImages(imagesData)
-    const cardImagesHtml = renderCardImages(allImages)
-    listImgContainer.innerHTML = cardImagesHtml 
-}   
+  listImgContainer.innerHTML = cardImagesHtml;
+});
 
+
+selectCategory.addEventListener("change", (event) => {
+   activeFilters.category = event.target.value;
+  const filtered = filterImages(activeFilters, imagesData);
+  // asignado valor que devuelve la funcion a la constante
+  const cardImagesHtml = renderCardImages(filtered);
+
+  // asignar el html al elemento obtenido con la funcion getElementById
+  listImgContainer.innerHTML = cardImagesHtml;
+});
