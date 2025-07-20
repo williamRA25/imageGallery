061c21356ae72ab7842bcc47d45c14e4c5722d08
williamRA25 <williamrodriguezavila43@gmail.com>
2025-07-20 18:39:23 -0500

    add clickable image card

--- assets/js/utilities/imgCard.js
+++ assets/js/utilities/imgCard.js
@@ -1,13 +1,13 @@
 function renderImageCard(image) {
   return `
-    <div class="col-6 col-md-4 col-lg-3 mb-4">
+    <a class="col-6 col-md-4 col-lg-3 mb-4 text-reset text-decoration-none" href="imageDetail.html?id=${image.id}">
       <div class="card" h-100>
         <img src="${image.url}" class="card-img-top image-card" alt="${image.description}">
         <div class="card-body">
          <p class="card-text text-muted"><b>Autor</b>: ${image.author}</p>
         </div>
       </div>
-    </div>
+    </a>
   `;
 }
 
